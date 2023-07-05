f3ef3f67d96c7f978653b8b8a599a0d421394095
NITHYA SATHEESH <nthsthsh21@gmail.com>
2023-07-05 05:59:29 +0530

    Add & display list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React,{ useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+const currentDate = new Date();
+const options = { weekday: 'long' };
+const dayOfWeek = currentDate.toLocaleDateString(undefined, options);
 
+const[toDos,settoDos] = useState([]);
+const[toDo,settoDo] = useState('');
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <h1 className='heading'>My ToDo List</h1>
+      <div className='subHeading'>
+        <h2>For {dayOfWeek}</h2>
+        <div className='input'>
+          <input value={toDo} onChange={(e)=>settoDo(e.target.value)}type="text" id="myInput" className="small-box" placeholder='Add your new ToDo...'/>
+          <div className='add'>
+            <i onClick={()=>settoDos([...toDos,{id:Date.now(),text: toDo,status:false}])} className="fa fa-plus-square" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <br/>
+        <br/>
+
+        <div className='todos'>
+
+         { toDos.map((obj)=>{
+          return(<div className='todo'>
+            <div className='left'>
+              <input onChange={(e)=>{
+                console.log(e.target.checked)
+                settoDos(toDos.filter(obj2=>{
+                  if(obj2.id==obj.id){
+                    obj2.status=e.target.checked
+                  }
+                  return obj2
+                }))
+              }} value={obj.status} type="checkbox" id="myCheckbox" name="myCheckbox" className='checkbox'/>
+              <p>{obj.text}</p>
+              <i className="fa fa-times"></i>
+            </div> 
+          </div>)
+        })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
